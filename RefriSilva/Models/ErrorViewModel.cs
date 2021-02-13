eebf25f3f79deb6b4167244fd74f11073843185e
André Mendes <andre.marcondes@betalearning.com.br>
2021-02-13 19:50:46 -0300

    criação projeto

---
+++ RefriSilva/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RefriSilva.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
